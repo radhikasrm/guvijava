08a9db26efa79a425b6bd3a8f1b6b4358a1317d4
radhikasrm <radhikashukla_shashi@srmuniv.edu.in>
2017-07-09 15:23:45 +0530

    Create summodd.java

---
+++ summodd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+
+class summodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i;
+		Scanner s=new Scanner(System.in);
+		System.out.println(" Enter any number:");
+		i=s.nextInt();
+		if(i%2==0)
+		{	 System.out.println("The entered number is even");
+		}
+		else
+ 
+		{
+			System.out.println("The entered number is odd");
+		}
+	}
+}
