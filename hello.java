157554aff5ffeefcd2ee8047ac2704b27af1df8d
radhikasrm <radhikashukla_shashi@srmuniv.edu.in>
2017-07-09 15:22:09 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello 
+{
+	public static void main(String args[])
+	{
+			System.out.println("Print HELLO five times!");
+			for(int i=0;i<=5;i++)
+			{
+				System.out.println("HELLO");
+			}
+	}
+}
